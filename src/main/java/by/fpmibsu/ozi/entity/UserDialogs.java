0546ae97f0b4b54fa3bad083361b11a96f6a75a6
Stanislav Zaycev <stas315172004@mail.ru>
2023-03-05 17:52:07 +0300

    Add UserDialogs

---
+++ src/main/java/by/fpmibsu/ozi/entity/UserDialogs.java
@@ -0,0 +1,23 @@
+package by.fpmibsu.ozi.entity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDialogs
+{
+    private Collection<Dialog> dialogs;
+
+    public UserDialogs()
+    {
+        dialogs = new ArrayList<>();
+    }
+
+    public UserDialogs(Collection<Dialog> dialogs)
+    {
+        this.dialogs = dialogs;
+    }
+
+    public Collection<Dialog> getDialogs() {
+        return dialogs;
+    }
+}
