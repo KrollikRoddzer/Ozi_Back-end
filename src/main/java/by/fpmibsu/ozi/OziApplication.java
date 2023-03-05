c4e864705b39dc5bdd3ce3fb76d189bc8f842b7a
Stanislav Zaycev <stas315172004@mail.ru>
2023-03-05 12:45:59 +0300

    Project initialization

---
+++ src/main/java/by/fpmibsu/ozi/OziApplication.java
@@ -0,0 +1,13 @@
+package by.fpmibsu.ozi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/fpmibsu/ozi/OziApplicationTests.java
@@ -0,0 +1,13 @@
+package by.fpmibsu.ozi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
